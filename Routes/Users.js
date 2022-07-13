6e93b94e72ff40039fc477f97deba1e582161a6f
root <root@162-240-56-117.bluehost.in>
2022-07-13 03:56:05 -0600

    first commit

---
+++ Routes/Users.js
@@ -0,0 +1,169 @@
+var express = require('express');
+var users = express.Router();
+var conn = require('../Database/database');
+var cors = require('cors')
+var jwt = require('jsonwebtoken');
+var token;
+
+
+users.use(cors());
+
+process.env.SECRET_KEY = "devesh";
+
+
+users.post("/register", function (req, res) {
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+  const email = req.body.email;
+  const password = req.body.password;
+  var today = new Date();
+
+  const query =
+    "INSERT INTO users (first_name, last_name ,email ,password, created) VALUES (?,?,?,?,?)";
+  conn.query(query, [first_name, last_name, email, password, today], function select(err, rows) {
+    if (err) {
+      res.json({
+        msg: err,
+      });
+    } else {
+      res.json({
+        msg: "success",
+      });
+    }
+  });
+});
+
+
+
+users.post('/login', function(req, res) {
+
+    var appData = {};
+    var email = req.body.email;
+    var password = req.body.password;
+
+    database.connection.getConnection(function(err, connection) {
+        if (err) {
+            appData["error"] = 1;
+            appData["data"] = "Internal Server Error";
+            res.status(500).json(appData);
+        } else {
+            connection.query('SELECT * FROM users WHERE email = ?', [email], function(err, rows, fields) {
+                if (err) {
+                    appData.error = 1;
+                    appData["data"] = "Error Occured!";
+                    res.status(400).json(appData);
+                } else {
+                    if (rows.length > 0) {
+                        if (rows[0].password == password) {
+                            let token = jwt.sign(rows[0], process.env.SECRET_KEY, {
+                                expiresIn: 1440
+                            });
+                            appData.error = 0;
+                            appData["token"] = token;
+                            res.status(200).json(appData);
+                        } else {
+                            appData.error = 1;
+                            appData["data"] = "Email and Password does not match";
+                            res.status(204).json(appData);
+                        }
+                    } else {
+                        appData.error = 1;
+                        appData["data"] = "Email does not exists!";
+                        res.status(204).json(appData);
+                    }
+                }
+            });
+            connection.release();
+        }
+    });
+});
+
+users.post('/login', function(req, res) {
+
+    var appData = {};
+    var email = req.body.email;
+    var password = req.body.password;
+
+    database.connection.getConnection(function(err, connection) {
+        if (err) {
+            appData["error"] = 1;
+            appData["data"] = "Internal Server Error";
+            res.status(500).json(appData);
+        } else {
+            connection.query('SELECT * FROM users WHERE email = ?', [email], function(err, rows, fields) {
+                if (err) {
+                    appData.error = 1;
+                    appData["data"] = "Error Occured!";
+                    res.status(400).json(appData);
+                } else {
+                    if (rows.length > 0) {
+                        if (rows[0].password == password) {
+                            token = jwt.sign(rows[0], process.env.SECRET_KEY, {
+                                expiresIn: 5000
+                            });
+                            appData.error = 0;
+                            appData["token"] = token;
+                            res.status(200).json(appData);
+                        } else {
+                            appData.error = 1;
+                            appData["data"] = "Email and Password does not match";
+                            res.status(204).json(appData);
+                        }
+                    } else {
+                        appData.error = 1;
+                        appData["data"] = "Email does not exists!";
+                        res.status(204).json(appData);
+                    }
+                }
+            });
+            connection.release();
+        }
+    });
+});
+
+users.use(function(req, res, next) {
+    var token = req.body.token || req.headers['token'];
+    var appData = {};
+    if (token) {
+        jwt.verify(token, process.env.SECRET_KEY, function(err) {
+            if (err) {
+                appData["error"] = 1;
+                appData["data"] = "Token is invalid";
+                res.status(500).json(appData);
+            } else {
+                next();
+            }
+        });
+    } else {
+        appData["error"] = 1;
+        appData["data"] = "Please send a token";
+        res.status(403).json(appData);
+    }
+});
+
+users.get('/getUsers', function(req, res) {
+
+    var appData = {};
+
+    database.connection.getConnection(function(err, connection) {
+        if (err) {
+            appData["error"] = 1;
+            appData["data"] = "Internal Server Error";
+            res.status(500).json(appData);
+        } else {
+            connection.query('SELECT *FROM users', function(err, rows, fields) {
+                if (!err) {
+                    appData["error"] = 0;
+                    appData["data"] = rows;
+                    res.status(200).json(appData);
+                } else {
+                    appData["data"] = "No data found";
+                    res.status(204).json(appData);
+                }
+            });
+            connection.release();
+        }
+    });
+});
+
+module.exports = users;
